a5cc0738dfee4704e41bc2e8d0955278aa6f7229
Nullumie <nullumie@gmail.com>
2026-07-05 12:13:39 -0300

    feat(core): integrate Jackson for JSON processing

---
+++ core/src/main/java/io/github/nullumie/corexie/core/json/Json.java
@@ -0,0 +1,79 @@
+/*
+ * SPDX-License-Identifier: LGPL-3.0-only
+ *
+ * Copyright (c) 2026 Nullumie
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.github.nullumie.corexie.core.json;
+
+import io.github.nullumie.corexie.core.json.module.SemVerJsonModule;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.json.JsonMapper;
+
+public final class Json {
+
+    private static final @NotNull String DEFAULT_NAME = "default";
+    private static final @NotNull Map<String, Json> instances = new ConcurrentHashMap<>();
+
+    static {
+        instances.put(
+                DEFAULT_NAME,
+                new Json(JsonMapper.builder().addModule(new SemVerJsonModule()).build()));
+    }
+
+    private final @NotNull JsonMapper mapper;
+
+    private Json(@NotNull JsonMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public static @NotNull Json get() {
+        return instances.get(DEFAULT_NAME);
+    }
+
+    public static @Nullable Json get(@NotNull String name) {
+        return instances.get(name);
+    }
+
+    public static @Nullable Json remove(@NotNull String name) {
+        if (DEFAULT_NAME.equalsIgnoreCase(name)) {
+            throw new IllegalArgumentException("Cannot remove the default JSON instance.");
+        }
+        return instances.remove(name);
+    }
+
+    public static @NotNull Json create(@NotNull String name, @NotNull JsonMapper mapper) {
+        if (DEFAULT_NAME.equalsIgnoreCase(name)) {
+            throw new IllegalArgumentException("Cannot overwrite the default JSON instance.");
+        }
+        Json json = new Json(mapper);
+        instances.put(name, json);
+        return json;
+    }
+
+    public void write(@NotNull OutputStream out, @NotNull Object value) throws JacksonException {
+        mapper.writeValue(out, value);
+    }
+
+    public @NotNull <T> T read(@NotNull InputStream src, @NotNull Class<T> clazz)
+            throws JacksonException {
+        return mapper.readValue(src, clazz);
+    }
+}
---
+++ core/src/main/java/io/github/nullumie/corexie/core/json/module/SemVerJsonModule.java
@@ -0,0 +1,68 @@
+/*
+ * SPDX-License-Identifier: LGPL-3.0-only
+ *
+ * Copyright (c) 2026 Nullumie
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.github.nullumie.corexie.core.json.module;
+
+import com.github.zafarkhaja.semver.ParseException;
+import com.github.zafarkhaja.semver.Version;
+import java.util.Objects;
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.JsonGenerator;
+import tools.jackson.core.JsonParser;
+import tools.jackson.databind.DeserializationContext;
+import tools.jackson.databind.SerializationContext;
+import tools.jackson.databind.ValueDeserializer;
+import tools.jackson.databind.ValueSerializer;
+import tools.jackson.databind.exc.MismatchedInputException;
+import tools.jackson.databind.module.SimpleModule;
+
+public final class SemVerJsonModule extends SimpleModule {
+
+    private static class Serializer extends ValueSerializer<Version> {
+        @Override
+        public void serialize(Version value, JsonGenerator gen, SerializationContext ctxt)
+                throws JacksonException {
+            Objects.requireNonNull(value, "Version value to serialize cannot be null");
+            gen.writeString(value.toString());
+        }
+    }
+
+    private static class Deserializer extends ValueDeserializer<Version> {
+        @Override
+        public Version deserialize(JsonParser p, DeserializationContext ctxt)
+                throws JacksonException {
+            String versionText = p.getString();
+
+            if (versionText == null || versionText.isBlank()) {
+                throw MismatchedInputException.from(
+                        p, Version.class, "SemVer string cannot be null or blank");
+            }
+
+            try {
+                return Version.parse(versionText);
+            } catch (ParseException e) {
+                throw ctxt.instantiationException(Version.class, e);
+            }
+        }
+    }
+
+    public SemVerJsonModule() {
+        super("JsonSemVerModule");
+        this.addSerializer(Version.class, new Serializer());
+        this.addDeserializer(Version.class, new Deserializer());
+    }
+}
